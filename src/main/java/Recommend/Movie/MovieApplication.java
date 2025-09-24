7a1a898ee2efa39b3491cdeb3849c9da720ad106
chear5967 <89283580+chear5967@users.noreply.github.com>
2025-09-24 23:46:01 +0900

    기존파일 빈칸 제거

--- src/main/java/Recommend/Movie/MovieApplication.java
+++ src/main/java/Recommend/Movie/MovieApplication.java
@@ -3,7 +3,6 @@ package Recommend.Movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class MovieApplication {
 
